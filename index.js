a9b6131f55610a99aaced07b7626049b6fbbd421
Ugrasenan V <36364057+ugrasenanv@users.noreply.github.com>
2022-01-27 01:14:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/* eslint-disable node/no-path-concat */
+const fallback = require('express-history-api-fallback')
+const express = require('express')
+const app = express()
+const root = `${__dirname}/dist`
+app.use(express.static(root))
+app.use(fallback('index.html', { root }))
+app.listen(process.env.PORT || 3000)
